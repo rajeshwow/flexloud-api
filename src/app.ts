f1f6a213ff34c677126a1e22f425efc91b05cd09
rajesh Prajapat <rprajapat@thb.co.in>
2026-06-07 20:59:53 +0530

    limit fix

--- src/app.ts
+++ src/app.ts
@@ -56,7 +56,7 @@ export function createApp() {
   app.disable("x-powered-by");
 
   app.use(helmet());
-  app.use(express.json({ limit: "1mb" }));
+  app.use(express.json({ limit: "15mb" }));
 
   const allowedOrigins = env.CORS_ORIGINS;
 
